f2c6ef5a3aeb0529640ca9d7c7d18a5d5adaef3f
limematheuz <mribeiro17.info@gmail.com>
2024-03-12 13:35:38 +0100

    ready

--- src/components/StormDamageComponents/StormIntro/StormIntro.jsx
+++ src/components/StormDamageComponents/StormIntro/StormIntro.jsx
@@ -7,7 +7,7 @@ export default function StormIntro() {
     <section className="storm-intro-container">
       <div className="storm-intro-title">
         <h2>
-          WE SUPPORT THE INSURANCE <span>COMPAN</span>
+          WE SUPPORT THE INSURANCE <span>COMPANY'S PROCEDURES</span>
         </h2>
       </div>
       <section className="storm-items">
